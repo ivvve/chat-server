710a1a7fd69bb86269fee4a52c5a51be640422b3
devson <sonyc5720@gmail.com>
2022-10-23 23:21:59 +0900

    configuration: DB 설정 추가

---
+++ src/main/kotlin/com/tistory/devs0n/chat/config/MongoDBConfiguration.kt
@@ -0,0 +1,30 @@
+package com.tistory.devs0n.chat.config
+
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.MongoTransactionManager
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+@EnableMongoAuditing
+@Configuration
+class MongoDBConfiguration {
+    @Bean
+    @ConfigurationProperties("spring.data.mongodb")
+    fun properties(): MongoProperties {
+        return MongoProperties()
+    }
+
+    @Bean
+    fun mongoClientDatabaseFactory(properties: MongoProperties): SimpleMongoClientDatabaseFactory {
+        return SimpleMongoClientDatabaseFactory(properties.uri)
+    }
+
+    @Bean
+    fun transactionManager(databaseFactory: MongoDatabaseFactory): MongoTransactionManager {
+        return MongoTransactionManager(databaseFactory)
+    }
+}
