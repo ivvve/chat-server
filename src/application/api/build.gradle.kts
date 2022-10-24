ae8b4e3f532079629d63a591aa5c2c77e3f1003b
devson <sonyc5720@gmail.com>
2022-10-24 22:48:08 +0900

    configuration: add springdoc-openapi for API documentation

--- src/application/api/build.gradle.kts
+++ src/application/api/build.gradle.kts
@@ -6,6 +6,10 @@ dependencies {
     // Web
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // Documentation
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
+
     // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
