7aa23b30b3f13d9acfefe32951b8fcb506d98ffc
devson <sonyc5720@gmail.com>
2022-10-24 22:51:33 +0900

    configuration: add `validation` dependency

--- src/application/api/build.gradle.kts
+++ src/application/api/build.gradle.kts
@@ -6,6 +6,9 @@ dependencies {
     // Web
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // Util
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     // Documentation
     implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
     implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
