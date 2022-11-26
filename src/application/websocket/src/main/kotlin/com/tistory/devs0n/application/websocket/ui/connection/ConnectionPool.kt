47ebc8f089406ae5c3fa34cf294bf9a54138bf27
devson <sonyc5720@gmail.com>
2022-11-26 14:41:04 +0900

    feat: add `ConnectionPool` to pooling connections

---
+++ src/application/websocket/src/main/kotlin/com/tistory/devs0n/application/websocket/ui/connection/ConnectionPool.kt
@@ -0,0 +1,17 @@
+package com.tistory.devs0n.application.websocket.ui.connection
+
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class ConnectionPool {
+    private val connections: MutableSet<Connection> = ConcurrentHashMap.newKeySet()
+
+    fun add(connection: Connection) {
+        this.connections.add(connection)
+    }
+
+    fun remove(connection: Connection) {
+        this.connections.remove(connection)
+    }
+}
