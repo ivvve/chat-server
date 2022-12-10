6844ee28b50e8e94b857b2b2df2d7d4a71ef6b3f
devson <sonyc5720@gmail.com>
2022-12-10 22:25:32 +0900

    feat: Java 19 Virtual Thread 설정

---
+++ src/application/api/src/main/kotlin/com/tistory/devs0n/application/api/config/TomcatApplicationConfiguration.kt
@@ -0,0 +1,26 @@
+package com.tistory.devs0n.application.api.config
+
+import org.apache.coyote.ProtocolHandler
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.AsyncTaskExecutor
+import org.springframework.core.task.support.TaskExecutorAdapter
+import java.util.concurrent.Executors
+
+@Configuration
+class TomcatApplicationConfiguration {
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    fun asyncTaskExecutor(): AsyncTaskExecutor {
+        return TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor())
+    }
+
+    @Bean
+    fun protocolHandlerVirtualThreadExecutorCustomizer(): TomcatProtocolHandlerCustomizer<*> {
+        return TomcatProtocolHandlerCustomizer { protocolHandler: ProtocolHandler ->
+            protocolHandler.executor = Executors.newVirtualThreadPerTaskExecutor()
+        }
+    }
+}
